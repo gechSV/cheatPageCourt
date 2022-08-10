2a8b753a317cb66bd97515572c52418deee82be0
“gechSV” <91822056+gechSV@users.noreply.github.com>
2022-08-10 10:13:08 +0900

    upd: DB request, disign

--- app/DataBase/modelDB.js
+++ app/DataBase/modelDB.js
@@ -82,14 +82,38 @@ Article.hasMany(many_Card_has_many_Articles)
 TableCategory.hasMany(Card)
 
 /**
- * Синхронизация моделей ORM и таблиц БД 
+ * Синхронизация моделей ORM и таблиц БД
  */
  function syncModel(){
-  sequelize.sync({force: true}).then(result=>{
+
+  sequelize.sync(/*{force: true}*/).then(result=>{
+    new Date();
+    console.log('11   '+Date.now())
     console.log('\033[95mМодели и таблицы БД синхронизированны.\033[0m')
     // console.log(result);
+    return
   })
   .catch(err=> console.log(err));
+}
+
+//
+//####### Добавление данных ####### 
+//
+
+/**
+ * Добавление строк в таблицу TableCategory
+ * 
+ * @param {*} name Название категории 
+ * @param {*} description Примечания
+ */
+function addTableCategoryItem(name, description){
+  TableCategory.create({
+    Name: name,
+    Descriptions: description
+  }).then(res=>{
+    console.log('\033[92mВ таблтцу \'TableCategory\' добавлена запись\033[0m')
+    console.log(res.dataValues)
+  }).catch(err=>console.log(err))
 }
 
 /**
@@ -100,14 +124,16 @@ TableCategory.hasMany(Card)
  * @param {*} appealPer Срок обжалования 
  * @param {*} reviewPer Срок рассмотрения
  * @param {*} notes Примечания
+ * @param {*} tableCategoryId id категории таблицы
  */
-function addCardItem(act, duty, appealPer, reviewPer, notes){
+function addCardItem(act, duty, appealPer, reviewPer, notes, tableCategoryId){
     Card.create({
       JudicialAct: act,
       Duty: duty, 
       AppealPeriod: appealPer,
       ReviewPeriod: reviewPer,
-      Notes: notes
+      Notes: notes,
+      TableCategoryId: tableCategoryId
     }).then(res=>{
       console.log('\033[92mВ таблтцу \'Card\' добавлена запись\033[0m')
       console.log(res.dataValues)
@@ -121,6 +147,8 @@ function addCardItem(act, duty, appealPer, reviewPer, notes){
  * @param {*} link Ссылка на источник
  */
 function addArticleItem(name, link){
+  new Date();
+    console.log('22    ' + Date.now())
   Article.create({
     Name: name, 
     Link: link
@@ -146,7 +174,21 @@ function addManyCardHasManyArticlesItem(cardId, articleId){
   }).catch(err=>console.log(err))
 }
 
+//
+//####### Запросы данных ####### 
+//
+
+function getAllCards(){
+  
+}
+
+
+
+
+
+
 exports.syncModel = syncModel
+exports.addTableCategoryItem = addTableCategoryItem
 exports.addCardItem = addCardItem
 exports.addArticleItem = addArticleItem
 exports.addManyCardHasManyArticlesItem = addManyCardHasManyArticlesItem
